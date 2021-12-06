974cbc2b8f23359a90d33ade199065ea4a704975
luftedar <ugurorcun91@gmail.com>
2021-12-06 12:14:07 +0300

    Add Book Progress / Book Summary

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,13 +3,21 @@ import React from 'react';
 function Book() {
   return (
     <div className="book-item">
-      <h5>Genre</h5>
-      <h2>Title</h2>
-      <p>Author</p>
-      <div>
-        <a href="http://localhost:3000/">Comments</a>
-        <a href="http://localhost:3000/">Remove</a>
-        <a href="http://localhost:3000/">Edit</a>
+      <div className="book-desc">
+        <h5>Genre</h5>
+        <h2>Title</h2>
+        <p>Author</p>
+        <div>
+          <a href="http://localhost:3000/">Comments</a>
+          <a href="http://localhost:3000/">Remove</a>
+          <a href="http://localhost:3000/">Edit</a>
+        </div>
+      </div>
+      <div className="progress" />
+      <div className="summary">
+        <h3>CURRENT CHAPTER</h3>
+        <h5>Chapter 17</h5>
+        <button type="button">Update Progress</button>
       </div>
     </div>
   );
