9cd8b7d2c6c190af38650fc40c6f810f41e16016
luftedar <ugurorcun91@gmail.com>
2021-12-06 12:21:45 +0300

    Add Form Section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/Header';
 import Progress from './components/Progress';
+import Form from './components/Form';
 
 function App() {
   return (
     <div>
       <Header />
       <Progress />
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Form() {
+  return (
+    <div>
+      <h3>ADD NEW BOOK</h3>
+      <form>
+        <input type="text" placeholder="Book title" />
+        <select name="Category">
+          <option>Category</option>
+          <option value="first">First</option>
+          <option value="second">Second</option>
+        </select>
+        <button type="button">ADD BOOK</button>
+      </form>
+    </div>
+  );
+}
+
+export default Form;
