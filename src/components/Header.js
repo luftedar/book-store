5529c1471c5dfc24df9177fcbb4cdb9f8905f724
luftedar <ugurorcun91@gmail.com>
2021-12-06 11:52:41 +0300

    Add Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="nav">
+      <h1>Bookstore CMS</h1>
+      <div className="nav-links">
+        <a href="www.google.com">Books</a>
+        <a href="www.google.com">Categories</a>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
