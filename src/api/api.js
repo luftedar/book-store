05f10a4738ecb3ebdcecdca662658c5d513fd680
luftedar <ugurorcun91@gmail.com>
2021-12-09 16:15:29 +0300

    Added Api Fetch/Post/Delete Functions

---
+++ src/api/api.js
@@ -0,0 +1,36 @@
+const URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/E0wnoGqC3httDh8BB8Bp/books';
+
+export const addBookToApi = async (book) => {
+  const newBook = await fetch(URL, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify({
+      item_id = book.id,
+      title: book.title,
+      category: book.category
+    })
+  });
+  return newBook;
+};
+
+export const removeBookFromApi = async (id) => {
+  const bookWillRemove = await fetch(URL, {
+    method: 'DELETE',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify({
+      item_id: id
+    })
+  });
+  return bookWillRemove;
+};
+
+export const getAllBooksFromApi = async () => {
+  const books = await fetch(URL).json(); //BURAYA DİKKAT
+  return books
+}
+
+export default { addBookToApi, removeBookFromApi, getAllBooksFromApi };
